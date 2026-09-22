da6f5c85d20cc0751cce8cc84ee52fdc18f5b2f3
Tim Stark <79977155+SuperStark@users.noreply.github.com>
2026-09-22 17:15:02 +0100

    Update custom text header in login page script

--- Logon/Files/Login-Page/retro-terminal/js/custom_text_header.js
+++ Logon/Files/Login-Page/retro-terminal/js/custom_text_header.js
@@ -1,16 +1,14 @@
-
- //Custom Text Header //
+//Custom Text Header //
  // ACSII slant font
 
 // Needs to be inside <section id="login" class="shadow">
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                     __                             ___       __  
-   ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__
-  / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/
- / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   
-/_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  
-                                                                 </pre> </div>
+<div class="custom-text-header"> <pre>   _____ __                   __          __  __(_)             
+  / ___// /_  ____ _  _____  / /__       / / / / /         ___  
+  \\__ \\/ __/ / __ '/ / ___/ / //_/      / /_/ / / | | / / / _ \\ 
+ ___/ / /_  / /_/ / / /    / ,&lt;        / __  / /  | |/ / /  __/ 
+/____/\\__/  \\__,_/ /_/    /_/|_|      /_/ /_/_/   |___/  \\___/  </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
