e0a7b032886aa5edeaf2e42aa4f087ec7f48dd85
Tim Stark <79977155+SuperStark@users.noreply.github.com>
2026-09-22 17:18:47 +0100

    Update custom_login.sh

--- Logon/Files/Login-Page/custom_login.sh
+++ Logon/Files/Login-Page/custom_login.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 TYPE="retro-terminal"
-THEME="green.css"
-DOMAIN="theme-park.dev"
-SCHEME="https"
+THEME="white.css"
+BASE_URL="https://raw.githubusercontent.com/SuperStark/Unraid/main/Logon/Files"
 ADD_JS="true"
 JS="custom_text_header.js"
 DISABLE_THEME="false"
 
 ## FAQ
 
-  # If you update the DOMAIN variable after the script has been run,
+  # If you update variables after the script has been run,
   # you must disable the whole theme with the DISABLE_THEME="true" env first and re-run it again after with "false".
-
-  # Your domain can be your custom domain or a github pages (user.github.io) etc. Don't add the whole URL with scheme, you only need to add the domain.
-    # If you are using your own github.io domain you must prepend /theme.park on all @imports/URLs in the css files. e.g. @import url(/theme.park/css/addons/unraid/login-page/retro-terminal/retro-terminal-base.css);
-
-  # If your custom domain uses a subfolder you must escape the forward slash. (custom.com\/subfolder)
 
   # If you are on an Unraid version older than 6.10 you need to update the LOGIN_PAGE variable to "/usr/local/emhttp/login.php"
 
-echo -e "Variables set:\\n\
-TYPE          = ${TYPE}\\n\
-THEME         = ${THEME}\\n\
-DOMAIN        = ${DOMAIN}\\n\
-SCHEME        = ${SCHEME}\\n\
-ADD_JS        = ${ADD_JS}\\n\
-JS            = ${JS}\\n\
-DISABLE_THEME = ${DISABLE_THEME}\\n"
+echo -e "Variables set:\n\
+TYPE          = ${TYPE}\n\
+THEME         = ${THEME}\n\
+BASE_URL      = ${BASE_URL}\n\
+ADD_JS        = ${ADD_JS}\n\
+JS            = ${JS}\n\
+DISABLE_THEME = ${DISABLE_THEME}\n"
 
 echo "NOTE: Change the LOGIN_PAGE variable to /usr/local/emhttp/login.php if you are on a version older than 6.10"
 LOGIN_PAGE="/usr/local/emhttp/webGui/include/.login.php"
@@ -51,44 +44,36 @@ if [ ! -f ${LOGIN_PAGE}.backup ]; then
   cp -p ${LOGIN_PAGE} ${LOGIN_PAGE}.backup
 fi
 
-# Use correct domain style
-case ${DOMAIN} in
-  *"github.io"*)
-  echo "Switching to github.io URL style"
-    DOMAIN="${DOMAIN}/theme.park"
-    ;;
-esac
-
 # Adding stylesheets
-if ! grep -q ${DOMAIN} ${LOGIN_PAGE}; then
+if ! grep -q "SuperStark" ${LOGIN_PAGE}; then
   echo "Adding stylesheet"
-  sed -i -e "\@<style>@i\    <link data-tp='theme' rel='stylesheet' href='${SCHEME}://${DOMAIN}/css/addons/unraid/login-page/${TYPE}/${THEME}'>" ${LOGIN_PAGE}
-  sed -i -e "\@<style>@i\    <link data-tp='base' rel='stylesheet' href='${SCHEME}://${DOMAIN}/css/addons/unraid/login-page/${TYPE}/${TYPE}-base.css'>" ${LOGIN_PAGE}
+  sed -i -e "\@<style>@i\    <link data-tp='theme' rel='stylesheet' href='${BASE_URL}/${TYPE}/${THEME}'>" ${LOGIN_PAGE}
+  sed -i -e "\@<style>@i\    <link data-tp='base' rel='stylesheet' href='${BASE_URL}/${TYPE}/${TYPE}-base.css'>" ${LOGIN_PAGE}
   echo 'Stylesheet set to' ${THEME}
 fi
 
 # Adding/Removing javascript
-if [ ${ADD_JS} = "true" ]; then
-  if ! grep -q ${JS} ${LOGIN_PAGE}; then
-    if grep -q "<script type='text/javascript' src='${SCHEME}://${DOMAIN}/css/addons/unraid/login-page/" ${LOGIN_PAGE}; then
+if [ "${ADD_JS}" = "true" ]; then
+  if ! grep -q "${JS}" ${LOGIN_PAGE}; then
+    if grep -q "<script type='text/javascript' src='${BASE_URL}" ${LOGIN_PAGE}; then
       echo "Replacing Javascript"
-      sed -i "/<script type='text\/javascript' src='${SCHEME}:\/\/${DOMAIN}\/css\/addons\/unraid\/login-page/c <script type='text/javascript' src='${SCHEME}://${DOMAIN}/css/addons/unraid/login-page/${TYPE}/js/${JS}'></script>" ${LOGIN_PAGE}
+      sed -i "\@<script type='text/javascript' src='${BASE_URL}@c\    <script type='text/javascript' src='${BASE_URL}/${TYPE}/js/${JS}'></script>" ${LOGIN_PAGE}
     else
       echo "Adding javascript"
-      sed -i -e "\@</body>@i\    <script type='text/javascript' src='${SCHEME}://${DOMAIN}/css/addons/unraid/login-page/${TYPE}/js/${JS}'></script>" ${LOGIN_PAGE}
+      sed -i -e "\@</body>@i\    <script type='text/javascript' src='${BASE_URL}/${TYPE}/js/${JS}'></script>" ${LOGIN_PAGE}
     fi
   fi
 else
-  if grep -q ${JS} ${LOGIN_PAGE}; then
+  if grep -q "${JS}" ${LOGIN_PAGE}; then
     echo "Removing javascript.."
-    sed -i "/<script type='text\/javascript' src='${SCHEME}:\/\/${DOMAIN}\/css\/addons\/unraid\/login-page/d" ${LOGIN_PAGE}
+    sed -i "\@<script type='text/javascript' src='${BASE_URL}@d" ${LOGIN_PAGE}
   fi
 fi
 
 # Changing stylesheet
-if ! grep -q ${TYPE}"/"${THEME} ${LOGIN_PAGE}; then
+if ! grep -q "${TYPE}/${THEME}" ${LOGIN_PAGE}; then
   echo "Changing existing custom stylesheet.." 
-  sed -i "/<link data-tp='theme' rel='stylesheet' href='${SCHEME}:\/\/${DOMAIN}\/css\/addons\/unraid\/login-page/c <link data-tp='theme' rel='stylesheet' href='${SCHEME}://${DOMAIN}/css/addons/unraid/login-page/${TYPE}/${THEME}'>" ${LOGIN_PAGE}
-  sed -i "/<link data-tp='base' rel='stylesheet' href='${SCHEME}:\/\/${DOMAIN}\/css\/addons\/unraid\/login-page/c <link data-tp='base' rel='stylesheet' href='${SCHEME}://${DOMAIN}/css/addons/unraid/login-page/${TYPE}/${TYPE}-base.css'>" ${LOGIN_PAGE}
+  sed -i "\@<link data-tp='theme'@c\    <link data-tp='theme' rel='stylesheet' href='${BASE_URL}/${TYPE}/${THEME}'>" ${LOGIN_PAGE}
+  sed -i "\@<link data-tp='base'@c\    <link data-tp='base' rel='stylesheet' href='${BASE_URL}/${TYPE}/${TYPE}-base.css'>" ${LOGIN_PAGE}
   echo 'Stylesheet set to' ${THEME}
 fi
